6bd1c25a7752fcf75f187415bf5e83d4b48d5367
Noel Niles <nniles@oceanit.com>
2020-02-10 22:44:31 -1000

    Really fix the time stuff
    I think the right way to do it is to be more aware of the local star.

--- src/imcommon.py
+++ src/imcommon.py
@@ -1,5 +1,6 @@
 import os
 from datetime import datetime
+from datetime import timezone
 from glob import glob
 from pathlib import Path
 
@@ -8,24 +9,22 @@ import pytz
 import tifffile
 from astral import LocationInfo
 from astral.sun import sun
+from astral import Observer
 
 
 def is_daylight(location):
-    utc      = pytz.UTC
-    city     = location['city_name']
-    region   = location['city_region']
-    timezone = location['timezone']
-    lat      = float(location['latitude'])
-    lon      = float(location['longitude'])
-    loc      = LocationInfo(name=city, region=region, timezone=timezone, latitude=lat, longitude=lon)
-    now      = utc.localize(datetime.utcnow())
-    today    = now.date()
-    s        = sun(loc.observer, date=today)
+    """True if it's daylight at the location."""
+    now   = datetime.now(timezone.utc)
+    lat   = float(location['latitude'])
+    lon   = float(location['longitude'])
+    obs   = Observer(latitude=lat, longitude=lon)
+    today = now.date()
+    s     = sun(obs, date=today)
 
     return s['sunrise'] <= now < s['sunset']
 
-
 def in_time_window(start, stop):
+    """True if the time is in between start and stop."""
     t = datetime.now()
 
     start_hour, start_minute = map(int, start.split(':'))
@@ -35,7 +34,6 @@ def in_time_window(start, stop):
     stop_time  = datetime(t.year, t.month, t.day)
 
     return start_time <= t < stop_time
-
 
 def ensure_directory_exists(path):
     """Make sure the archive path exists."""
