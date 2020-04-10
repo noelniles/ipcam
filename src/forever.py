3811213c59c2c47f7c520371ae51c405765b98e0
Noel Niles <noelniles@gmail.com>
2020-04-09 16:26:55 -1000

    WIP

--- src/forever.py
+++ src/forever.py
@@ -1,11 +1,23 @@
 #!/usr/bin/env python
+import argparse
 import sys
 from subprocess import Popen
 
-config   = sys.argv[1]
+def cli():
+    ap = argparse.ArgumentParser()
+    ap.add_argument('--config', type=str, required=True,
+        help='absolute path to config file')
+    ap.add_argument('--process', type=str, required=True,
+        help='the process to run forever')
 
-while True:
-    cmd = ['./ipcam.py', '--config_file', config]
+    return cli()
+
+def main():
+    args = cli()
+
+    cmd = [sys.executable, args.process, '--config_file', args.config]
     p = Popen(cmd)
     p.wait()
 
+if __name__ == '__main__':
+    main()
