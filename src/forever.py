885aba1a24561615b845194d04da9986ff074093
Noel Niles <noelniles@gmail.com>
2020-04-09 15:40:27 -1000

    WIP

--- src/forever.py
+++ src/forever.py
@@ -2,12 +2,9 @@
 import sys
 from subprocess import Popen
 
-
-filename = sys.argv[1]
 config   = sys.argv[2]
 
 while True:
-    print('Starting ', filename)
     cmd = ['./ipcam.py', '--config_file', config]
     p = Popen(cmd)
     p.wait()
