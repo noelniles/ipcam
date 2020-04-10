074dac984ae0b1f2759367335c90e70f2c2af71e
Noel Niles <noelniles@gmail.com>
2020-04-09 15:47:02 -1000

    WIP

--- systemd/setup_service.sh
+++ systemd/setup_service.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 SYSD_PATH=/etc/systemd/system/
 sudo cp ibeach.service $SYSD_PATH
-sudo cp ibeach-email.service $SYSD_PATH
+sudo cp ibeach-notifier@.service $SYSD_PATH
 
 sudo systemctl daemon-reload
-sudo systemctl start ibeach-email.service
+sudo systemctl start ibeach-notifier@.service
 sudo systemctl start ibeach.service
 
-sudo systemctl status ibeach-email.service
+sudo systemctl status ibeach-notifier@.service
 sudo systemctl status ibeach.service
