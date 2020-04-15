85f5ac96105ecb1d234f2c4aea4dd80de016cbde
Noel Niles <nniles@oceanit.com>
2020-04-14 20:03:01 -1000

    WIP

--- src/consumer.py
+++ src/consumer.py
@@ -10,3 +10,6 @@ def consume(queues, callback):
         for r in can_read:
             item = r.get()
             callback(item.camera_id, item.timestamp, item.frameno, item.image)
+            del item
+
+
