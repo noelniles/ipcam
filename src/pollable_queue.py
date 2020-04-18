c538df35b196e28770ac00b45a51a2d73b464686
Noel Niles <nniles@oceanit.com>
2020-04-18 09:13:01 -1000

    WIP

--- src/pollable_queue.py
+++ src/pollable_queue.py
@@ -1,9 +1,10 @@
 import queue
 import socket
 import os
+from collections import deque
 
 
-class PollableQueue(queue.Queue):
+class PollableQueue(deque):
     def __init__(self, maxsize=10):
         super().__init__(maxsize=maxsize)
         self._putsocket, self._getsocket = socket.socketpair()
@@ -12,9 +13,9 @@ class PollableQueue(queue.Queue):
         return self._getsocket.fileno()
 
     def put(self, item):
-        super().put(item)
+        super().append(item)
         self._putsocket.send(b'x')
 
     def get(self):
         self._getsocket.recv(1)
-        return super().get()
+        return super().popleft()
