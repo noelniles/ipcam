ec97327f4979e2e6891f2b667dc596f272f14ee6
Noel Niles <nniles@oceanit.com>
2020-04-29 10:21:19 -1000

    WIP

--- src/ipcam.py
+++ src/ipcam.py
@@ -3,21 +3,14 @@ import argparse
 import logging
 import sys
 import time
-from guppy import hpy
 from pathlib import Path
-from queue   import Queue
 from datetime import datetime
 from systemd.journal import JournaldLogHandler
-from threading import Thread
 
 from archive  import Archive
 from cameras  import CameraConfig, CameraList, Camera
-from consumer import consume
-from imcommon import in_time_window
-from imcommon import is_daylight
-from pollable_queue import PollableQueue
+from imcommon import in_time_window, is_daylight
 
-H = hpy()
 
 logger  = logging.getLogger(__name__)
 handler = JournaldLogHandler()
@@ -26,17 +19,6 @@ handler.setFormatter(fmt)
 logger.addHandler(handler)
 logger.setLevel(logging.DEBUG)
 
-class BufferItem:
-    def __init__(self, timestamp, frameno, camera_id, image):
-        self.timestamp = timestamp
-        self.frameno   = frameno
-        self.camera_id = camera_id
-        self.image     = image
-
-class Frame:
-    _fields_ = [
-        ('timestamp')
-    ]
 def cli():
     ap = argparse.ArgumentParser()
     ap.add_argument('--config_file', type=str, required=True, help='path to a config file')
@@ -44,34 +26,27 @@ def cli():
     return ap.parse_args()
 
 def main(args):
-    confs    = CameraConfig(args.config_file)
-    ncams    = confs.n_cameras()
-    cams     = CameraList()
-    archive  = Archive(confs)
-    framenos = [0]*ncams
-    location = confs.data['location']
-    sleep    = confs.data['cameras'][0]['sleep']
+    confs       = CameraConfig(args.config_file)
+    ncams       = confs.n_cameras()
+    cams        = CameraList()
+    archive     = Archive(confs)
+    framenos    = [0]*ncams
+    location    = confs.data['location']
+    sleep       = confs.data['cameras'][0]['sleep']
     start_video = confs.data['cameras'][0]['start_video']
-    stop_video = confs.data['cameras'][0]['stop_video']
-
-    #q = PollableQueue()
-    #vq = PollableQueue()
+    stop_video  = confs.data['cameras'][0]['stop_video']
 
     for c in confs.data['cameras']:
         camid = c['id']
         addr  = c['url']
-        cam = Camera(addr, camid)
+        cam   = Camera(addr, camid)
         cams.append(cam)
         framenos[cam.camid] = int(archive.next_prefix(cam.camid))
-    
-    #image_thread = Thread(target=consume, args=(q, archive.save_image))
-    #video_thread = Thread(target=consume, args=(vq, archive.save_video))
-    #image_thread.start()
-    #video_thread.start()
 
     while True:
         if is_daylight(location) or args.debug:
             is_video_time = in_time_window(start_video, stop_video)
+
             for i, cam in enumerate(cams):
                 im        = cam.grab()
                 timestamp = datetime.now()
@@ -81,19 +56,15 @@ def main(args):
                     continue
 
                 framenos[i] += 1
-                #buffer_item = BufferItem(timestamp, framenos[i], cam.camid, im)
                 if is_video_time or args.debug:
-                    #q.put(buffer_item)
                     archive.save_video(cam.camid, timestamp, framenos[i], im)
                 else:
                     archive.save_image(cam.camid, timestamp, framenos[i], im)
-                    #vq.put(buffer_item)
 
                 del im
             
             if not is_video_time or not args.debug:
                 time.sleep(sleep)
-
 
 if __name__ == '__main__':
     args  = cli()
--- systemd/email_notifier.sh
+++ systemd/email_notifier.sh
@@ -4,7 +4,7 @@ APP=/home/pi/git/ipcam/src/notifier.py
 $PYTHON_EXE $APP \
     --email          "pilikia.ibeach@gmail.com" \
     --username       "pilikia.ibeach@gmail.com" \
-    --keyring_system 'ibeach-email'             \
+    --dotfile        '/home/pi/git/ipcam/config/credentials.txt'             \
     --smtp           'smtp.gmail.com:587'       \
     --sms_gateways   'nniles@oceanit.com'       \
     --service        'ibeach'
